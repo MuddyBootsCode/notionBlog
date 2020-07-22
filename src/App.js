346cbb0a3e0426ee5bce46e8de9057aeff1e42b4
muddybootscode <firsttogo98@gmail.com>
2020-07-22 07:27:03 -0400

    Pulling directly from notion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import "react-notion/src/styles.css";
 import { NotionRenderer } from "react-notion";
-import { pageChunk } from './chunk';
 
 function App() {
-  // const [data, setData] = useState(null);
-  //
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try{
-  //       const result = await fetch('https://notion-api.splitbee.io/v1/page/b2a847477c554189b48ad2ab26d37fb7')
-  //       const blockMap = await result.json();
-  //       setData(blockMap)
-  //     } catch(error){
-  //       console.log(error)
-  //     }
-  //   }
-  //   fetchData();
-  // }, [])
+  const [data, setData] = useState(null);
 
-  const pageData = pageChunk.recordMap.block;
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const result = await fetch('https://notion-api.splitbee.io/v1/page/b2a847477c554189b48ad2ab26d37fb7')
+        const blockMap = await result.json();
+        setData(blockMap)
+      } catch(error){
+        console.log(error)
+      }
+    }
+    fetchData();
+  }, [])
+
+  const pageData = data || {};
 
   return (
     <div className="App">
